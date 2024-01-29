0a78ba4e83afd8c2b9937ed1ee05bac893e53e8f
SHUBAM CHAUDHARY <110082517+shubamchaudhary@users.noreply.github.com>
2024-01-29 16:59:23 +0530

    Create Days.java

---
+++ Days.java
@@ -0,0 +1,24 @@
+SnakeGame.java:41: error: cannot find symbol
+                    restartGame();
+                    ^
+  symbol: method restartGame()
+SnakeGame.java:55: error: cannot find symbol
+                    if (isDirectionSafe(preferredHorizontalMove, x[0], y[0])) {
+                        ^
+  symbol: method isDirectionSafe(char,int,int)
+SnakeGame.java:59: error: cannot find symbol
+                    if (isDirectionSafe(preferredVerticalMove, x[0], y[0])) {
+                        ^
+  symbol: method isDirectionSafe(char,int,int)
+SnakeGame.java:65: error: cannot find symbol
+        startGame();
+        ^
+  symbol:   method startGame()
+  location: class SnakeGame
+SnakeGame.java:83: error: cannot find symbol
+        game.draw(g);
+            ^
+  symbol:   method draw(Graphics)
+  location: variable game of type SnakeGame
+5 errors
+PS C:\Users\1037706\OneDrive - Blue Yonder\Desktop\JAVA>
